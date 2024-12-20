e1a3558f4ff2da9fd6611f8f1b4cf712434c37e7
AllanNguyen <allandog06@gmail.com>
2024-12-20 00:30:40 -0800

    Hosting static page with firebase and database hosted by freemysqlhosting

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const fs = require("fs");
-const { connect } = require("http2");
 const mysql = require("mysql2");
-const { stripVTControlCharacters } = require("util");
+const functions = require("firebase-functions")
 
 const app = express();
 app.use(express.text());
 
-app.use("/js", express.static("./public/js"));
-app.use("/css", express.static("./public/css"));
-app.use("/html", express.static("./app/html"));
+app.use("/js", express.static("../public/js"));
+app.use("/css", express.static("../public/css"));
+app.use("/html", express.static("../app/html"));
 
-app.delete("/delete", function(req, res) {
+function createDatabaseConnection() {
     const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "main"
+        host: "sql3.freemysqlhosting.net",
+        user: "sql3753170",
+        password: "VHRpPDihr4",
+        database: "sql3753170",
+        port: 3306
     });
+
+    return connection;
+}
+
+app.delete("/delete", function (req, res) {
+    const connection = createDatabaseConnection();
 
     connection.query(
         `DELETE FROM user WHERE id=${req.body}`,
-        function(err, result) {
+        function (err, result) {
             if (err) throw err;
 
             res.send();
@@ -29,13 +35,8 @@ app.delete("/delete", function(req, res) {
     )
 });
 
-app.post("/add", function(req, res) {
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "main"
-    });
+app.post("/add", function (req, res) {
+    const connection = createDatabaseConnection();
 
     connection.connect(function (error) {
         if (error) throw error;
@@ -56,13 +57,8 @@ app.post("/getData", function (req, res) {
     if (req.body == "undefined" || req.body.length == 0) {
         nameFilter = "1=1";
     }
-    
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "main"
-    });
+
+    const connection = createDatabaseConnection();
 
     connection.connect(function (error) {
         if (error) throw error;
@@ -77,33 +73,28 @@ app.post("/getData", function (req, res) {
     });
 });
 
-app.post("/updateData", function(req, res) {
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "main"
-    });
+app.post("/updateData", function (req, res) {
+    const connection = createDatabaseConnection();
 
-    connection.connect(function(error){
+    connection.connect(function (error) {
         if (error) throw error;
-        
+
         const json = JSON.parse(req.body);
 
         connection.query(
             `UPDATE user SET name="${json.newName}" WHERE id="${json.id}"`,
-            function(err) {
+            function (err) {
                 if (err) throw err;
 
                 res.send();
-            }  
+            }
         );
     });
 });
 
 app.get("/", function (req, res) {
-    let document = fs.readFileSync("./app/html/main.html", "utf8");
-
+    let document = fs.readFileSync("../public/main.html", "utf8");
+    
     res.send(document);
 });
 
@@ -113,7 +104,10 @@ app.use(function (req, res) {
         .send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
 });
 
-let port = 8000;
-app.listen(port, function () {
-    console.log("listening on port: " + port);
-});
+// let port = 8000;
+// app.listen(port, function () {
+//     console.log("listening on port: " + port);
+// });
+
+exports.app = functions.https.onRequest(app);
+
